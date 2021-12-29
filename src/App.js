80fb8d268824faf4734f3ada8a4e9f51000de9fa
Melike Çalış <87120287+C9201MLK@users.noreply.github.com>
2021-12-29 16:53:32 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from "react";
-import About from "./pages/About";
-import Header from "./pages/Header";
-import Pro from "./pages/Pro";
+
+import FlagApp from "./pages/FlagApp";
 
 const App = () => {
   return (
     <div>
-      <h1>APP</h1>
-      <Header />
-      <About />
-      <Pro />
+      <FlagApp />
     </div>
   );
 };
