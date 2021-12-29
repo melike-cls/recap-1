5e59762fd91ba257a80e3cefdd4ab541da494542
Melike Çalış <87120287+C9201MLK@users.noreply.github.com>
2021-12-29 14:53:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import About from "./pages/About";
+import Header from "./pages/Header";
+import Pro from "./pages/Pro";
+
+const App = () => {
+  return (
+    <div>
+      <h1>APP</h1>
+      <Header />
+      <About />
+      <Pro />
+    </div>
+  );
+};
+
+export default App;
